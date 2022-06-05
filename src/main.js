012d1810f521dbc82f3378811fc6653b857b675d
Andrew Wei <weiandrew2012@gmail.com>
2022-06-05 00:51:14 -0700

    grading comments in main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+/*
+HELLVETICA
+    - made by: Andrew Wei, Aidan Barcia-Bacon, Manoj Sara
+    - music: Sophia Eisenbach
+
+GRADING:
+    batter:
+        1.) no critical error/crashes; game sometimes run really slow on chrome but that's 100% a phaser problem
+        2.) have a restart, title, and ending
+        3.) have tutorials on the ground
+        4.) with moderate skill game can be completed. Have a level select for grades and debug keys for going to levels/scenes with keys 1-7 on title screen
+        5.) have comments and well structured code
+        6.) we have a ton of pulls/pushes on github
+    bake:
+        1.) have a lot of work put into artistic cohesion
+        2.) game revolves around core mechanic
+
+    icing:
+        1.) our game has a ton of effort and polish put into it for a good experience
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 1400,
